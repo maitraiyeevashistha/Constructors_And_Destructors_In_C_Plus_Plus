1d9b920d0039c1e6604fef1151c4d7794a1707a2
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-08-28 12:07:37 +0530

    Create Destructors Example - 2.cpp

---
+++ Destructors Example - 2.cpp
@@ -0,0 +1,37 @@
+// EXPERIMENT-12
+// MAITRAIYEE VASHISTHA
+// 24070123057
+// ENTC A3
+
+#include <iostream>
+using namespace std;
+
+class Date{
+public:
+int d = 26;
+int m = 8;
+int y = 2023;
+
+~Date(){
+    cout<<"Destructor Called !"<<endl;
+};
+};
+
+int main(){
+    Date d1,d2,d3,d4;
+    int i;
+    for(i=0;i<4;i++){
+        Date d1;
+    }
+}
+
+/*Output :
+Destructor Called !
+Destructor Called !
+Destructor Called !
+Destructor Called !
+Destructor Called !
+Destructor Called !
+Destructor Called !
+Destructor Called !
+*/
