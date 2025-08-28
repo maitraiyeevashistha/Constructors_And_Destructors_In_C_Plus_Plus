9c3b02b023dba0fc1db0ee790cb9513b3eef45ef
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-08-28 11:56:39 +0530

    Create Destructors.cpp

---
+++ Destructors.cpp
@@ -0,0 +1,36 @@
+// EXPERIMENT-12
+// MAITRAIYEE VASHISTHA
+// 24070123057
+// ENTC A3
+
+#include <iostream>
+using namespace std;
+
+int count = 0;
+
+class destruct{
+public:
+destruct(){
+    count ++;
+    cout<<"No of objects created : "<<count<<endl;
+}
+~destruct(){
+    count--;
+    cout<<"No. of objects destroyed : "<<count<<endl;
+}
+};
+
+int main(){
+    destruct aa,bb,cc,dd;
+}
+
+/*Output :
+No of objects created : 1
+No of objects created : 2
+No of objects created : 3
+No of objects created : 4
+No. of objects destroyed : 3
+No. of objects destroyed : 2
+No. of objects destroyed : 1
+No. of objects destroyed : 0
+*/
